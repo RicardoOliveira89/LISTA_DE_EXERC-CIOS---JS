94e54a69ad5e65746d25a5148c975838523c375b
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 18:17:05 -0300

    Update 07.js
    Resposta 07.js

---
+++ Comandos de Entrada e Saída/07.js
@@ -0,0 +1,9 @@
+// 7. Escreva um programa que leia o valor da base e da altura de um triângulo 
+// e calcule a sua área.
+
+let base = 4;
+let altura = 6;
+
+let area = (base * altura) / 2;
+
+console.log("Área = " + area);
