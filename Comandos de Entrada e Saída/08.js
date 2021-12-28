b1f18233d6da5a18c8836a7454a200790c05eb21
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 18:24:39 -0300

    Create 08.js
    Resposta 08.js

---
+++ Comandos de Entrada e Saída/08.js
@@ -0,0 +1,9 @@
+// 8. Escreva um programa que leia o valor de uma temperatura em Celsius 
+// e calcule o seu valor correspondente em Fahrenheit e em Kelvin.
+
+let celsius = 25;
+
+let fahrenheit = (celsius * 1.8) + 32;
+let kelvin = celsius + 273.15;
+
+console.log("\nFahrenheit = " + fahrenheit + "\nKelvin = " + kelvin + "\n");
