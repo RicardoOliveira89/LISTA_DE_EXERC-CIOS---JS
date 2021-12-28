ca02c2ea99ee4f00a5a657f0368e321817fd4085
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 14:39:42 -0300

    Create 04.js
    Resposta 04.js

---
+++ Comandos de Entrada e Saída/04.js
@@ -0,0 +1,12 @@
+// 4. Escreva um programa que leia o número inteiro positivo e calcule 
+// o seu dobro, o seu triplo, o seu quadrado, o seu cubo e a sua raiz quadrada.
+
+let numero = 3;
+
+let dobro = numero * 2;
+let triplo = numero * 3;
+let quadrado = Math.pow(numero, 2);
+let cubo = Math.pow(numero,3);
+let raizQuadrada = Math.sqrt(numero,2);
+
+console.log("\nDobro = " + dobro + "\nTriplo = " + triplo + "\nQuadrado = " + quadrado + "\nCubo = " + cubo + "\nRaiz Quadrada = " + raizQuadrada + "\n");
