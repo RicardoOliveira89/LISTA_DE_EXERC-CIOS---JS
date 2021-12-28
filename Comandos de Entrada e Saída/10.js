5f8a82b4ad008884493789946aa234ed6b8a604a
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 19:09:05 -0300

    Create 10.js
    Resposta 10.js

---
+++ Comandos de Entrada e Saída/10.js
@@ -0,0 +1,9 @@
+// 10. Escreva um programa que leia o valor dos dois catetos de um triângulo retângulo e
+// calcule o valor da hipotenusa.
+
+let cateto1 = 3;
+let cateto2 = 2;
+
+let hipotenusa = Math.sqrt((Math.pow(cateto1, 2) + Math.pow(cateto2,2)), 2);
+
+console.log("\nHipotenusa = " + hipotenusa +"\n");
