fec55c48687332e374c219de41add44379d837cc
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 14:21:34 -0300

    Create 03
    Resposta da questão 03

---
+++ Comandos de Entrada e Saída/03.js
@@ -0,0 +1,8 @@
+// 3. Escreva um programa que leia o valor do lado de um quadrado e calcule a sua área e o seu perímetro.
+
+let lado = 6;
+let area = lado * lado;
+
+let perimetro = lado * 4;
+
+console.log("Área = " + area + "\nPerímetro = " + perimetro);
