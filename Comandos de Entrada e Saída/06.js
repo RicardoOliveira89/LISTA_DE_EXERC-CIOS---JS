3264a1f40f6c4891969aeabfcf530fd9bdc00aec
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 17:56:08 -0300

    Create 06.js
    Resposta 06.js

---
+++ Comandos de Entrada e Saída/06.js
@@ -0,0 +1,9 @@
+// 6. Escreva um programa que leia um número inteiro e informe 
+// o seu antecessor e o seu sucessor.
+
+let numero = 10;
+
+let antecessor = numero - 1;
+let sucessor = numero + 1;
+
+console.log("\nAntecessor = " + antecessor + "\nSucessor = " + sucessor + "\n");
