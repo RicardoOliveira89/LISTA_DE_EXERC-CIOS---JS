79204d42e89e56d092cf0a8fd52898c83cd4eb3c
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 12:34:54 -0300

    Create 01.js
    Resposta da questão  01

---
+++ 01.js
@@ -0,0 +1,9 @@
+// 1) Escreva um programa que leia três números inteiros e calcule a sua média aritmética.
+
+let num1 = 8;
+let num2 = 5;
+let num3 = 2;
+
+let media = (num1 + num2 + num3) / 3;
+
+console.log(media);
