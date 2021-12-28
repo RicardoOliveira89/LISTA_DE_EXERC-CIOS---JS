ad7d23e7a042fa462a9eb08ac3a14ccc7f3fbb00
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 18:30:40 -0300

    Create 09.js
    Resposta 09.js

---
+++ Comandos de Entrada e Saída/09.js
@@ -0,0 +1,8 @@
+// 9. Escreva um programa que leia o valor de uma distância percorrida em km e o tempo
+// gasto em horas e calcule a velocidade média em m/s.
+
+let kmHoras = 160;
+
+let metrosSegundos = kmHoras / 3.6;
+
+console.log("\n" + metrosSegundos + "ms");
