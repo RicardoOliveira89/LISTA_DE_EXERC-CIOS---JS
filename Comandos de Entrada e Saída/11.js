686e3b99f11677eab2e7e59ed9088a52782a7e59
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 21:10:05 -0300

    Create 11.js
    Resposta 11.js

---
+++ Comandos de Entrada e Saída/11.js
@@ -0,0 +1,8 @@
+// 11. Escreva um programa que leia um valor em real, a cotação atual do dólar 
+// e calcule o valor informado pelo usuário em dólares.
+
+let real = 100;
+
+let dolar = real / 5.63;
+
+console.log("\nValor em dólar = U$ " + dolar + "\n");
