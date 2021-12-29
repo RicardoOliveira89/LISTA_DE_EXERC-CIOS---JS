42c8b3d8290725f8ef55299e8343b97717a89e64
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 21:15:54 -0300

    Create 12.js
    Resposta 12.js

---
+++ Comandos de Entrada e Saída/12.js
@@ -0,0 +1,9 @@
+// 12. Escreva um programa que leia um valor em polegadas e o converta para centímetros,
+// considerando que uma polegada equivale a 2.54 cm.
+
+const Umapolegada = 2.54;
+
+let polegada = 15;
+let centimetros = polegada * Umapolegada;
+
+console.log("\nCentímetros = " + centimetros + "\n");
