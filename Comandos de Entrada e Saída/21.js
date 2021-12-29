22b95a551df1ee7c75577d6ec68cbae231d13b7f
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-29 15:10:21 -0300

    Create 21.js
    Resposta 21.js

---
+++ Comandos de Entrada e Saída/21.js
@@ -0,0 +1,9 @@
+// 21. Escreva um programa que leia o preço de produção de um determinado produto e a
+// margem de lucro desejada pelo fabricante e calcule qual deve ser o seu preço de venda.
+
+let precoProducao = 90;
+let margemLucroFabricante = 30;
+
+let precoVenda = (((precoProducao * margemLucroFabricante) / 100) + precoProducao);
+
+console.log(precoVenda);
