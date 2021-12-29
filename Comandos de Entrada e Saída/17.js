3e18e5546117b984a242b8082d1ed05051393363
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-29 09:53:45 -0300

    Create 17.js

---
+++ Comandos de Entrada e Saída/17.js
@@ -0,0 +1,10 @@
+// 17. Escreva um programa que leia a área de uma casa e o valor cobrado 
+// por um arquiteto por cada metro quadrado de um projeto arquitetônico 
+// e calcule o custo final do projeto.
+
+let area = 115;
+let valorMetroQuadrados = 50;
+
+let valorProjeto = area * valorMetroQuadrados;
+
+console.log("\nCusto final do Projeto arquitetônico: R$ " + valorProjeto +"\n");
