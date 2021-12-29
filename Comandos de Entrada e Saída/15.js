42219913c5400f24c0fc44c8e37714e4182c045e
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-28 23:14:30 -0300

    Create 15.js
    Resposta da 15.js

---
+++ Comandos de Entrada e Saída/15.js
@@ -0,0 +1,9 @@
+// 15. Escreva um programa que leia uma palavra e um número inteiro k e identifique 
+// a k-ésima letra da palavra informada pelo usuário.
+
+let palavra = "Ricardo";
+let k = 5;
+
+let kesima = palavra[k-1];
+
+console.log("\nK-ésima letra é " + kesima);
