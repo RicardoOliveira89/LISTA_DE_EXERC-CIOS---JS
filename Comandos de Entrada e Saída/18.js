40bec8838a43dacd781ebc3d7a46261f9be46065
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-29 10:42:47 -0300

    Create 18.js
    Resposta 18.js

---
+++ Comandos de Entrada e Saída/18.js
@@ -0,0 +1,7 @@
+//18. Escreva um programa que leia o valor do seno de um ângulo e calcule o valor do seu cosseno.
+
+let seno = 3;
+
+let cosseno = Math.cos(seno);
+
+console.log("\nCosseno: " + cosseno + "\n");
