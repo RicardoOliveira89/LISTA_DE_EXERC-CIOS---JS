3353d856bb7d63075d662147d90dd02a6d1645af
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-29 14:38:07 -0300

    Create 19.js
    Resposta 19.js

---
+++ Comandos de Entrada e Saída/19.js
@@ -0,0 +1,9 @@
+// 19. Escreva um programa que leia os valores de dois ângulos internos de um triângulo e
+// calcule o valor do terceiro ângulo.
+
+let anguloInterno1 = 80;
+let anguloInterno2 = 65;
+
+let anguloInterno3 = 180 - (anguloInterno1 + anguloInterno2);
+
+console.log ("\nTerceiro ângulo: " + anguloInterno3 + "\n");
