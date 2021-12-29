51601b85a8b30b65b198bd2d284009be227e1acd
Ricardo Oliveira <ricardooliveiradev@gmail.com>
2021-12-29 09:38:10 -0300

    Create 16.js
    Resposta 16.js

---
+++ Comandos de Entrada e Saída/16.js
@@ -0,0 +1,7 @@
+// 16. Escreva um programa que leia uma letra minúscula e imprima a sua letra maiúscula correspondente.
+
+let letra = "r";
+
+let maiuscula = letra.toUpperCase();
+
+console.log("\nMaiúscula: " + maiuscula + "\n");
